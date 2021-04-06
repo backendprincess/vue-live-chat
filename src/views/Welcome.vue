8011067bb1441a25e80f5f02f3ab5adcd6392c7a
backendprincess <lemur.king.julien@gmail.com>
2021-04-06 20:52:33 +0300

    Created Login form

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,13 +2,15 @@
     <div class="welcome container">
         <p>Welcome</p>
         <SignupForm />
+        <LoginForm />
     </div>
 </template>
 
 <script>
 import SignupForm from '../components/SignupForm'
+import LoginForm from '../components/LoginForm'
 export default {
-    components: { SignupForm }
+    components: { SignupForm, LoginForm }
 }
 </script>
 
@@ -16,5 +18,25 @@ export default {
     .welcome {
         text-align: center;
         padding: 20px 0;
+    }
+
+    .welcome form {
+        width: 300px;
+        margin: 20px auto;
+    }
+
+    .welcome label {
+        display: block;
+        margin: 20px 0 10px;
+    }
+
+    .welcome input {
+        width: 100%;
+        padding: 10px;
+        border-radius: 20px;
+        border: 1px solid #eee;
+        outline: none;
+        color: #999;
+        margin: 10px auto;
     }
 </style>
