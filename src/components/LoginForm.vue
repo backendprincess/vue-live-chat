4dc5f6535daae854e6900002880a25712b425dbc
backendprincess <lemur.king.julien@gmail.com>
2021-04-06 21:17:25 +0300

    Created Login composable

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -9,22 +9,29 @@ vue<template>
                placeholder="password"
                v-model="password">
         <button>Log in</button>
+        <div class="error">{{ error }}</div>
     </form>
 </template>
 
 <script>
 import { ref } from 'vue'
+import useLogin from '../composables/useLogin'
 
 export default {
     setup() {
         const email = ref('');
         const password = ref('');
 
-        const handleSubmit = () => {
-            console.log(email.value, password.value)
+        const { error, login } = useLogin()
+
+        const handleSubmit = async () => {
+            await login(email.value, password.value)
+            if (!error.value) {
+                console.log('User logged in')
+            }
         }
 
-        return { email, password, handleSubmit }
+        return { email, password, handleSubmit, error }
     }
 }
 </script>
