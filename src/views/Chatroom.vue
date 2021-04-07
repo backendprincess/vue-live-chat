17b4ab1fe66aa7f8195d1b19514c248036293645
backendprincess <lemur.king.julien@gmail.com>
2021-04-07 11:46:56 +0300

    Add a Chat window

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container">
         <Navbar />
+        <ChatWindow />
         <NewChatFormVue />
     </div>
 </template>
@@ -11,9 +12,10 @@ import getUser from '../composables/getUser'
 import { useRouter } from 'vue-router'
 import { watch } from 'vue'
 import NewChatFormVue from '../components/NewChatForm.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 
 export default {
-    components: { Navbar, NewChatFormVue },
+    components: { Navbar, NewChatFormVue, ChatWindow },
     setup() {
         const router = useRouter()
         const { user } = getUser()
